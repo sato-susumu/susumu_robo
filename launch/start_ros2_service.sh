b87667b23a3af79011c9f6f495188069c7bad794
sato-susumu <75652942+sato-susumu@users.noreply.github.com>
2025-01-19 12:35:12 +0900

    Revert "Enabled audio launch"
    This reverts commit 5abecf74b824cf68d4eaf7744bada52950c72fff.

--- launch/start_ros2_service.sh
+++ launch/start_ros2_service.sh
@@ -2,6 +2,6 @@ sudo systemctl start ros2_bringup
 sudo systemctl start ros2_mid360
 sudo systemctl start ros2_ddsm115
 sudo systemctl start ros2_realsense
-sudo systemctl start ros2_audio
+# sudo systemctl start ros2_audio
 sudo systemctl start ros2_led
 
--- launch/stop_ros2_service.sh
+++ launch/stop_ros2_service.sh
@@ -2,6 +2,6 @@ sudo systemctl stop ros2_bringup
 sudo systemctl stop ros2_mid360
 sudo systemctl stop ros2_ddsm115
 sudo systemctl stop ros2_realsense
-sudo systemctl stop ros2_audio
+# sudo systemctl stop ros2_audio
 sudo systemctl stop ros2_led
 
