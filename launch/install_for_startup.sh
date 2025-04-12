04aa35290234705b33f6bc7f3180676f5f8220a3
sato-susumu <75652942+sato-susumu@users.noreply.github.com>
2025-04-12 11:06:57 +0900

    Launch関連のスクリプト名変更

--- launch/install_for_startup.sh
+++
@@ -1,7 +0,0 @@
-ros2 run robot_upstart install --job ros2_bringup --systemd-after nss-lookup.target --setup $HOME/ros2_ws/install/setup.bash --symlink susumu_robo/launch/ros2_bringup.launch.py
-ros2 run robot_upstart install --job ros2_mid360 --systemd-after nss-lookup.target --setup $HOME/ros2_ws/install/setup.bash --symlink susumu_robo/launch/ros2_mid360.launch.py
-ros2 run robot_upstart install --job ros2_ddsm115 --systemd-after nss-lookup.target --setup $HOME/ros2_ws/install/setup.bash --symlink susumu_robo/launch/ros2_ddsm115.launch.py
-ros2 run robot_upstart install --job ros2_realsense --systemd-after nss-lookup.target --setup $HOME/ros2_ws/install/setup.bash --symlink susumu_robo/launch/ros2_realsense.launch.py
-# ros2 run robot_upstart install --job ros2_audio --systemd-after sound.target --setup $HOME/ros2_ws/install/setup.bash --symlink susumu_robo/launch/ros2_audio.launch.py
-ros2 run robot_upstart install --job ros2_led --systemd-after nss-lookup.target --setup $HOME/ros2_ws/install/setup.bash --symlink susumu_robo/launch/ros2_led.launch.py
-
